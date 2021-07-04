796d24788929959cafd418db2dffa4d4f53175a7
kusa-mochi <whoatemyapplepie@gmail.com>
2021-07-04 17:19:28 +0900

    make basic page layout on the app.

--- flexible-reversi/src/App.vue
+++ flexible-reversi/src/App.vue
@@ -1,8 +1,40 @@
 <template>
   <div id="app">
-    <router-view />
+    <div class="app-container">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  color: black;
+  box-sizing: border-box;
+}
+
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
+
+#app {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: stretch;
+
+  width: 100%;
+  height: 100%;
+}
+
+.app-container {
+  max-width: 800px;
+  width: 100%;
+  height: 100%;
+  background-color: #eeeeee;
+}
 </style>
