32b9343c930762d0d3453b5726dae7e6d2ca364f
kusa-mochi <whoatemyapplepie@gmail.com>
2021-07-11 00:41:33 +0900

    add animation on turn over stones.

--- flexible-reversi/src/components/ReversiCell.vue
+++ flexible-reversi/src/components/ReversiCell.vue
@@ -1,14 +1,20 @@
 <template>
   <div @click="$emit('click')" class="reversi-cell">
     <div v-if="state === 0"></div>
-    <div v-else-if="state === 1" class="stone stone--black"></div>
-    <div v-else-if="state === 2" class="stone stone--white"></div>
+    <div v-else-if="state === 1 || state === 2" :class="stoneStyle"></div>
     <div v-else-if="state === 3" class="wall"></div>
   </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    stoneStyle: {
+      get() {
+        return `stone stone--${this.state === 1 ? "black" : "white"}`;
+      },
+    },
+  },
   name: "ReversiCell",
   props: {
     state: {
@@ -37,6 +43,7 @@ export default {
   border: 2px solid black;
   width: 90%;
   height: 90%;
+  transition: background-color 0.3s ease-out;
 
   &--black {
     background-color: #505050;
