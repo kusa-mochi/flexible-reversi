2d871569a4736d6900b57973767ed9b9fb3ab0ab
kusa-mochi <whoatemyapplepie@gmail.com>
2021-08-22 21:48:48 +0900

    TTL support for connection ID

--- server-side/lambda/flexibleReversiConnect/lambda_function.py
+++ server-side/lambda/flexibleReversiConnect/lambda_function.py
@@ -1,16 +1,24 @@
 import boto3
+import datetime
 import json
 
 dynamodb = boto3.resource('dynamodb')
 connections = dynamodb.Table('flexible-reversi-connections')
 
 def lambda_handler(event, context):
-    connection_id = event.get('requestContext', {}).get('connectionId')
+    connectionId = event.get('requestContext', {}).get('connectionId')
     domainName = event.get('requestContext',{}).get('domainName')
     stage       = event.get('requestContext',{}).get('stage')
     
     endpointUrl = F"https://{domainName}/{stage}"
-    result = connections.put_item(Item={'id': connection_id, 'endpointUrl': endpointUrl})
+    expirationDatetime = datetime.datetime.now() + datetime.timedelta(days=2)
+    result = connections.put_item(
+        Item={
+            'id': connectionId,
+            'endpointUrl': endpointUrl,
+            'expirationDatetime': int(expirationDatetime.timestamp())
+        }
+    )
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
