6a282dafb7cf248e1e7b6a89b2c36e5c308cebec
kusa-mochi <whoatemyapplepie@gmail.com>
2021-09-01 18:57:29 +0900

    install vuex persistedstate.

--- flexible-reversi/src/store/index.js
+++ flexible-reversi/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
@@ -78,4 +79,10 @@ export default new Vuex.Store({
   },
   actions: {},
   modules: {},
+  plugins: [
+    createPersistedState({
+      key: "flexible-reversi",
+      storage: window.sessionStorage,
+    }),
+  ],
 });
