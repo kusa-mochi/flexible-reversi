94ab4f966b4b615587706a2f56e3a69c2399f6ae
kusa-mochi <whoatemyapplepie@gmail.com>
2021-09-05 12:15:33 +0900

    make new stone type: reversible stone.

--- flexible-reversi/src/components/ReversiCell.vue
+++ flexible-reversi/src/components/ReversiCell.vue
@@ -1,8 +1,14 @@
 <template>
   <div @click="$emit('click')" class="reversi-cell">
     <div v-if="state === 0"></div>
-    <div v-else-if="state === 1 || state === 2" :class="stoneStyle"></div>
+    <div v-else-if="state === 1" class="stone stone--black"></div>
+    <div v-else-if="state === 2" class="stone stone--white"></div>
     <div v-else-if="state === 3" class="wall"></div>
+    <div v-else-if="state === 4" class="reversible-stone">
+      <div class="reversible-stone__black"></div>
+      <div class="reversible-stone__white"></div>
+      <div class="reversible-stone__border"></div>
+    </div>
   </div>
 </template>
 
@@ -34,11 +40,13 @@ export default {
   justify-content: center;
   align-items: center;
 
+  position: relative;
   width: 100%;
   height: 100%;
 }
 
 .stone {
+  position: relative;
   border-radius: 50%;
   border: 2px solid black;
   width: 90%;
@@ -54,8 +62,47 @@ export default {
 }
 
 .wall {
+  position: relative;
   width: 100%;
   height: 100%;
   background-color: #505050;
+}
+
+.reversible-stone {
+  position: relative;
+  width: 90%;
+  height: 90%;
+  transform: rotate(-45deg);
+
+  &__black,
+  &__white,
+  &__border {
+    position: absolute;
+    left: 0;
+  }
+
+  &__black {
+    top: 0;
+    width: 100%;
+    height: 50%;
+    background-color: #505050;
+    border: none;
+    border-radius:50% 50% 0 0/100% 100% 0 0;
+  }
+  &__white {
+    top: 50%;
+    width: 100%;
+    height: 50%;
+    background-color: #f5f5f5;
+    border: none;
+    border-radius:0 0 50% 50% /0 0 100% 100%;
+  }
+  &__border {
+    width: 100%;
+    height: 100%;
+    background-color: transparent;
+    border-radius: 50%;
+    border: 2px solid black;
+  }
 }
 </style>
